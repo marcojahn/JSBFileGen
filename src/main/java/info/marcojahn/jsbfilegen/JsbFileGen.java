165713e1ae90319360c1a1164d36ca7d3e0b65e0
Marco Jahn <marco.jahn@gmail.com>
2012-07-24 13:23:25 +0200

    added build file; added main class

---
+++ src/main/java/info/marcojahn/jsbfilegen/JsbFileGen.java
@@ -0,0 +1,20 @@
+/*
+ * (C)opyright 2012 ssb Software Service und Beratung GmbH
+ */
+package info.marcojahn.jsbfilegen;
+
+/**
+ * @author Marco Jahn <marco.jahn@prodyna.de>
+ * 
+ */
+public class JsbFileGen {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("running JsbFileGen");
+
+    }
+}
