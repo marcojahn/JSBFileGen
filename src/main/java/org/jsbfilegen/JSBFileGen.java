c61200ace5aec3c550e991e2e8180f7974417762
Marco Jahn <marco.jahn@gmail.com>
2012-07-30 18:45:59 +0200

    added basic cmd line arguments

--- src/main/java/org/jsbfilegen/JSBFileGen.java
+++ src/main/java/org/jsbfilegen/JSBFileGen.java
@@ -3,6 +3,10 @@
  */
 package org.jsbfilegen;
 
+import jargs.gnu.CmdLineParser;
+import jargs.gnu.CmdLineParser.IllegalOptionValueException;
+import jargs.gnu.CmdLineParser.UnknownOptionException;
+
 import java.util.List;
 
 import org.jsbfilegen.parser.JSFileParser;
@@ -17,17 +21,46 @@ public class JSBFileGen {
 
     public static void main(String[] args) {
 
-        final JSBFileGenOptions options = new JSBFileGenOptions();
-        options.setProject("EPlanung");
-        options.setSourceLocation("../EPlanung/src/main");
-        options.setNameSpace("disoweb.apvertrieb.webclient.eplanung");
+        CmdLineParser cmdLineParser = new CmdLineParser();
 
-        JSFileParser parser = new JSFileParser(options);
+        try {
+            cmdLineParser.parse(args);
+            CmdLineParser.Option helpOpt = cmdLineParser.addBooleanOption('h', "help");
 
-        List<JSFileInfo> infos = parser.parse();
-        for (JSFileInfo info : infos) {
-            System.out.println(info);
+            Boolean help = (Boolean) cmdLineParser.getOptionValue(helpOpt);
+            if (help != null && help.booleanValue()) {
+                usage();
+                System.exit(0);
+            }
+
+            final JSBFileGenOptions options = new JSBFileGenOptions();
+            options.setProject("EPlanung");
+            options.setSourceLocation("../EPlanung/src/main");
+            options.setNameSpace("disoweb.apvertrieb.webclient.eplanung");
+
+            JSFileParser parser = new JSFileParser(options);
+
+            List<JSFileInfo> infos = parser.parse();
+            for (JSFileInfo info : infos) {
+                System.out.println(info);
+            }
+
+        } catch (IllegalOptionValueException e) {
+            e.printStackTrace();
+            usage();
+            System.exit(1);
+        } catch (UnknownOptionException e) {
+            e.printStackTrace();
+            usage();
+            System.exit(1);
         }
+
     }
 
+    private static void usage() {
+
+        System.err.println("\nUsage: java -jar JSBFileGen.jar [options]\n\n" +
+
+        "Global Options\n" + "  -h, --help                 Displays this information\n");
+    }
 }
